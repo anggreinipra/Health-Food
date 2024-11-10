cc35bb591432083b9bc8d79fc8e094ce4c5dfc3f
anggreinipra <anggreinipra@gmail.com>
2024-11-10 11:46:38 +0700

    create navbar, add style and js for toggle menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Toggle class active
+const navbarNav = document.queryselector(".navbar-nav");
+
+//onclick hamburger menu
+document.queryselector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
